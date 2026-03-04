b985be89789cb4c89e0b52e096dbcdcb617cf201
pawarmah <pawarmahadeo11@gmail.com>
2026-03-04 13:35:05 +0000

    file added

---
+++ hi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "I have restarted my journey as Devops Engg"
+
+echo "Regards"
+echo "Mahadeo Pawar"
--- pass.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-read -sp "Enter the password : " pass
-echo -e "\nYour Password is : $pass"
